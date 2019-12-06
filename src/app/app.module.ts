6b0f6e15180963a2973294a3fabaa7ec3f4b3e60
David Aruldass <davidva@byui.edu>
2019-12-06 09:01:24 -0700

    added addmovies component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HomeComponent } from './home/home.component';
 import { BollywoodComponent } from './bollywood/bollywood.component';
 import { HollywoodComponent } from './hollywood/hollywood.component';
 import { KollywoodComponent } from './kollywood/kollywood.component';
+import { AddmoviesComponent } from './addmovies/addmovies.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { KollywoodComponent } from './kollywood/kollywood.component';
     HomeComponent,
     BollywoodComponent,
     HollywoodComponent,
-    KollywoodComponent
+    KollywoodComponent,
+    AddmoviesComponent
   ],
   imports: [
     BrowserModule,
